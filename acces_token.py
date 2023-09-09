e16c56b20a215cbc4faddf873e03e17284a7fdd1
SlowBoke <tsukkomi@mail.ru>
2023-09-10 01:37:27 +0400

    catching new message and replying

---
+++ acces_token.py
@@ -0,0 +1,6 @@
+
+
+token = 'vk1.a.IewvPVaJmDlBSDzr_OPGL9cUumAtiGmayKh3sXTkaHjb1BJ9' \
+        'qTaq0cMdkaAQfKeXqcrYEvA40Xdot-1Lh1bID69QZLO19LIvtHWGRI' \
+        'NIe_f4Uqm1Ouw9GVgnBchBSx0E3L0GeZZX2UJLRJavtDLe3S9Z-qrx' \
+        'pqkF0mQ21XiF1tHe6PWPVmz7zxKK-M-BifdFLCB9Zk2ctVxJb1L3A-YAWQ'
--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+# -*- coding: utf-8 -*-
+from acces_token import token
+from random import randint
+import vk_api
+import vk_api.bot_longpoll
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+group_id = 222488942
 
 
-# Press the green button in the gutter to run the script.
+class VkBot:
+
+    def __init__(self, group_id, access_token):
+        self.g_id = group_id
+        self.token = access_token
+
+        self.vk = vk_api.VkApi(token=self.token)
+        self.vk_methods = self.vk.get_api()
+        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=self.g_id, wait=25)
+
+    def run(self):
+        for event in self.long_poller.listen():
+            if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
+                from_text = event.object.message['text']
+                print('принял')
+                self.vk_methods.messages.send(user_id=event.object.message['from_id'],
+                                              random_id=randint(0, 2 ** 20),
+                                              message=f'Йоу, мистер Уайт. Твоё сообщение - "{from_text}"')
+            else:
+                print('Coming soon...')
+
+
+def main():
+    vk_bot = VkBot(group_id=group_id, access_token=token)
+    vk_bot.run()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
