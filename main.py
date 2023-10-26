f29d4d7e4d8e7e3225d7cfd5b49e9962d05ebc4a
SlowBoke <tsukkomi@mail.ru>
2023-10-26 23:41:27 +0400

    added intention check, scenarios with handlers. Bot has been provided with logic to deal with it

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 
-
+import handlers
+import settings
 from random import randint
-import vk_api
-import vk_api.bot_longpoll
+from vk_api import VkApi
 from log_config import main_log
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 
 try:
     from settings import TOKEN, GROUP_ID
 except ImportError:
     exit('Do cp settings.py.default to settings.py and set token and group_id')
+
+
+class UserState:
+    """User's state inside a scenario"""
+    def __init__(self, scenario_name, step_name, context=None):
+        self.scenario_name = scenario_name
+        self.step_name = step_name
+        self.context = context or {}
 
 
 class VkBot:
@@ -26,15 +35,60 @@ class VkBot:
         """
         self.token = access_token
         self.group_id = group_id
-        self.vk = vk_api.VkApi(token=self.token)
+        self.vk = VkApi(token=self.token)
         self.vk_methods = self.vk.get_api()
-        self.long_poller = vk_api.bot_longpoll.VkBotLongPoll(vk=self.vk, group_id=self.group_id, wait=25)
+        self.long_poller = VkBotLongPoll(vk=self.vk, group_id=self.group_id, wait=25)
+        self.user_states = dict()
 
     def run(self):
         """Running the bot"""
         for event in self.long_poller.listen():
             main_log.info('Bot gets an event.')
             self.event_handler(event=event)
+
+    def message_new_event(self, event):
+        user_id = event.object.message['peer_id']
+        text = event.object.message['text']
+
+        if user_id in self.user_states:
+            text_to_send = self.continue_scenario(user_id=user_id, text=text)
+        else:
+            for intent in settings.INTENTS:
+                if any(token in text for token in intent['tokens']):
+                    if intent['answer']:
+                        text_to_send = intent['answer']
+                    else:
+                        text_to_send = self.start_scenario(user_id=user_id, scenario_name=intent['scenario'])
+                    break
+            else:
+                text_to_send = settings.DEFAULT_ANSWER
+        return text_to_send
+
+    def start_scenario(self, user_id, scenario_name):
+        scenario = settings.SCENARIOS[scenario_name]
+        first_step = scenario['first_step']
+        cur_step = scenario['steps'][first_step]
+        text_to_send = cur_step['text']
+        self.user_states[user_id] = UserState(scenario_name=scenario_name, step_name=first_step)
+        return text_to_send
+
+    def continue_scenario(self, user_id, text):
+        state = self.user_states[user_id]
+        steps = settings.SCENARIOS[state.scenario_name]['steps']
+        step = steps[state.step_name]
+
+        handler = getattr(handlers, step['handler'])
+        if handler(text=text, context=state.context):
+            next_step = steps[step['next_step']]
+            text_to_send = next_step['text'].format(**state.context)
+            if next_step['next_step']:
+                state.step_name = step['next_step']
+            else:
+                self.user_states.pop(user_id)
+        else:
+            text_to_send = step['failure_text'].format(**state.context)
+
+        return text_to_send
 
     def event_handler(self, event):
         """
@@ -43,13 +97,13 @@ class VkBot:
         :param event: VkBotMessageEvent object
         :return: None
         """
-        if event.type == vk_api.bot_longpoll.VkBotEventType.MESSAGE_NEW:
-            from_text = event.object.message['text']
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            text_to_send = self.message_new_event(event=event)
             main_log.info("Bot gets an user's message.")
             self.vk_methods.messages.send(
                 user_id=event.object.message['from_id'],
                 random_id=randint(0, 2 ** 20),
-                message=f'Йоу, мистер Уайт. Твоё сообщение - "{from_text}"'
+                message=text_to_send
             )
             main_log.info('Bot sends message back to user.')
         else:
--- tests.py
+++ tests.py
@@ -6,6 +6,38 @@ from unittest.mock import patch, Mock
 from main import VkBot
 
 
+event = {
+    'group_id': 222488942,
+    'type': 'message_new',
+    'event_id': 'c20a08d9098dd212cd586598ad84dd27f43586fe',
+    'v': '5.131',
+    'object': {
+        'message': {
+            'entity_version': 0,
+            'date': 1698328326,
+            'from_id': 308611803,
+            'id': 41,
+            'out': 0,
+            'attachments': [],
+            'conversation_message_id': 39,
+            'fwd_messages': [],
+            'important': False,
+            'is_hidden': False,
+            'peer_id': 308611803,
+            'random_id': 0,
+            'text': 'Кек'
+        },
+        'client_info': {
+            'button_actions': ['text', 'vkpay', 'open_app', 'location', 'open_link',
+                               'open_photo', 'callback', 'intent_subscribe', 'intent_unsubscribe'],
+            'keyboard': True,
+            'inline_keyboard': True,
+            'carousel': True,
+            'lang_id': 0
+        }
+    }
+}
+
 class Test1(TestCase):
     def test_vkbot_run(self):
         count = 5
@@ -15,8 +47,8 @@ class Test1(TestCase):
         long_poller_listen_mock = Mock()
         long_poller_listen_mock.listen = long_poller_mock
 
-        with patch('main.vk_api.VkApi'):
-            with patch('main.vk_api.bot_longpoll.VkBotLongPoll', return_value=long_poller_listen_mock):
+        with patch('main.VkApi'):
+            with patch('main.VkBotLongPoll', return_value=long_poller_listen_mock):
                 bot = VkBot('', '')
                 bot.event_handler = Mock()
                 bot.run()
@@ -24,3 +56,11 @@ class Test1(TestCase):
                 bot.event_handler.assert_called()
                 bot.event_handler.assert_any_call(event=obj)
                 assert bot.event_handler.call_count == count
+
+    def test_vkbot_event_handler(self):
+        event = None
+
+
+        with patch('main.VkApi'):
+            with patch('main.VkBotLongPoll'):
+                pass
